8fb2a961f80b2036c068f74577346b76dbff0f50
luj <132845125+Akary015@users.noreply.github.com>
2023-05-07 22:34:50 +0300

    luj bot
    ╰⋆𝗟𝗨𝗚⺌🧜🏻‍♀️✧.

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201008510107'] //ur owner number
+global.ownername = "LUJ❤️" //ur owner name
+global.ytname = "YT: luj bot" //ur yt chanel name
+global.socialm = "GitHub: LUJ" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
+global.ownernomer = "201008510107" //ur number
+global.premium = ['201008510107'] //ur premium number
+global.botname = '❤️LUJ❤️' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣لج حبيب⍣" //ur sticker watermark packname
+global.author = "⍣201008510107⍣" //ur sticker watermark author
+global.wm = "❤️LUJ❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
